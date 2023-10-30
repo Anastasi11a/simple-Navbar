dcc6a36563a21ada81390c46d98153a9ac6b9d54
Anastasiia <felitsias22@gmail.com>
2023-10-30 15:37:10 +0100

    Added function MyImg

--- mysimplenavbar/src/App.js
+++ mysimplenavbar/src/App.js
@@ -4,12 +4,13 @@ import {Routes, Route, Link} from 'react-router-dom';
 import Homepage from './components/Homepage';
 import AboutMe from './components/AboutMe';
 import Contact from './components/Contact';
-
+import MyImage from './components/MyImage';
 
 function App() {
   return (
     <div className='containerApp'>
       <nav className='nav'>
+        <MyImage />
         <Link to='/' className='navItem'>Homepage</Link>
         <Link to='/about-me' className='navItem'>About me</Link>
         <Link to='/contact' className='navItem'>Contact</Link>
