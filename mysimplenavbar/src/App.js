2e1a4198c2178afb1917c644c4703efb3a5391c8
Anastasiia <felitsias22@gmail.com>
2023-10-30 11:50:22 +0100

    Added a simple Navbar

---
+++ mysimplenavbar/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './index.css';
+import {Routes, Route, Link} from 'react-router-dom';
+import Homepage from './components/Homepage';
+import AboutMe from './components/AboutMe';
+import Contact from './components/Contact';
+
+
+function App() {
+  return (
+    <div className='containerApp'>
+      <nav className='nav'>
+        <Link to='/' className='navItem'>Homepage</Link>
+        <Link to='/about-me' className='navItem'>About me</Link>
+        <Link to='/contact' className='navItem'>Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={ <Homepage /> } />
+        <Route path='/about-me' element={ <AboutMe /> } />
+        <Route path='/contact' element={ <Contact /> } />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
