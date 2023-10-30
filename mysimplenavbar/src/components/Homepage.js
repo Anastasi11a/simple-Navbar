e69c1c0a84272a7690a0917dd27ca08855e41f55
Anastasiia Alex <119977051+Anastasi11a@users.noreply.github.com>
2023-10-30 16:40:48 +0100

    Update Homepage.js
    update Homepage

--- mysimplenavbar/src/components/Homepage.js
+++ mysimplenavbar/src/components/Homepage.js
@@ -1,6 +1,11 @@
 function Homepage() {
+    const myTxtStyle = {
+        textShadow: '2px 2px 5px #051624e7'
+    };
+
     return (
-        <h1>Welcome to my site!</h1>
+        <h1 style={myTxtStyle} 
+        >Welcome to my site!</h1>
     );
 }
 
